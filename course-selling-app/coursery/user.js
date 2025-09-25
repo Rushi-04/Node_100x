a0cf0e6d893f5b866fb271bae3d956e857c982c8
Rushi-04 <borkarananta028@gmail.com>
2025-09-25 23:10:46 +0530

    added user routes

--- course-selling-app/coursery/user.js
+++ course-selling-app/coursery/user.js
@@ -12,7 +12,81 @@ const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 const userAuth = require("./middlewares/user");
 
 
+userRouter.post('/signup', async (req, res) => {
 
+    const requiredBody = z.object({
+        email: z.email(),
+        firstName: z.string().min(2).max(100),
+        lastName: z.string().min(2).max(100),
+        password: z.string().min(6).max(30)
+    });
+
+    const parsedData = requiredBody.safeParse(req.body);
+
+    if(!parsedData.success)
+    {
+        res.json({
+            message: "Incorrect format",
+            error: parsedData.error
+        })
+        return
+    }
+    
+    const {email, firstName, lastName, password} = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 6)
+
+    await userModel.create({
+        email: email,
+        firstName: firstName,
+        lastName: lastName,
+        password: hashedPassword
+    });
+
+    res.json({
+        message: "User Sign-Up Successfull"
+    });
+
+});
+
+userRouter.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({
+        email: email
+    });
+
+    const passwordMatched = bcrypt.compare(password, user.password);
+
+    if(passwordMatched){
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_USER_PASSWORD);
+
+        res.json({
+            token: token
+        });
+    }else{
+        res.status(403).json({
+            message : "Invalid Credentials"
+        })
+    }
+
+});
+
+
+userRouter.get('/purchases', userAuth, async (req, res) => {
+    const userId = req.userId;
+
+    const purchases = await purchaseModel.find({
+       userId: userId 
+    });
+
+    return res.json({
+        message: "User Purchased Courses",
+        purchases: purchases
+    });
+});
 
 
 
