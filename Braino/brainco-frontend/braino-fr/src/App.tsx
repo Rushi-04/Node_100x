0dd7f0d630eace5aab391251b32929447aaa6004
Rushikesh Borkar <borkarananta028@gmail.com>
2025-12-24 22:15:59 +0530

    configured frontend

---
+++ Braino/brainco-frontend/braino-fr/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='bg-neutral-400 flex w-full h-screen justify-center items-center'>
+        <h1 className='text-center text-black font-bold text-3xl'>Hello from Rushi</h1>
+      </div>
+  )
+}
+
+export default App
