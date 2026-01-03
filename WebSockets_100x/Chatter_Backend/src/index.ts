28949990c2751bb11719df3fe6adb338d7936856
Rushikesh Borkar <borkarananta028@gmail.com>
2026-01-03 17:47:15 +0530

    Room Based Chat

--- WebSockets_100x/Chatter_Backend/src/index.ts
+++ WebSockets_100x/Chatter_Backend/src/index.ts
@@ -28,28 +28,98 @@
 // --------- Chatter: Broadcast Chat Application ------------
 
 
+// import { WebSocketServer, WebSocket } from "ws";
+
+// const wss = new WebSocketServer({port: 8080});
+
+// let allSockets: WebSocket[] = [];
+// let user = 0;
+
+// wss.on("connection", (socket) => {
+//     allSockets.push(socket);
+//     user++;
+
+//     console.log("#"+user+" Connected to WebSocket.")
+    
+//     socket.on("message", (msg) => {
+//         allSockets.forEach((skt) => {
+//             skt.send("Msg from Server: "+msg.toString());
+//         })
+//     });
+
+
+//     socket.on("close", () => {
+//         allSockets = allSockets.filter(x => x !== socket)
+//     })
+// });
+
+// -------------- Room Based : Chat application ------------
+
+/*
+
+Join Schema 
+{
+    "type": "join",
+    "payload": {
+        "roomId": 121
+    }
+}
+
+Chat Schema 
+{
+    "type": "chat",
+    "payload": {
+        "message": "Kas Kay Mandali !"
+    }
+}
+
+*/
+
 import { WebSocketServer, WebSocket } from "ws";
 
+// websocket server
 const wss = new WebSocketServer({port: 8080});
 
-let allSockets: WebSocket[] = [];
-let user = 0;
+interface User {
+    socket: WebSocket,
+    room: string
+}
 
+// 
+let allSockets: User[] = [];
+
+// on connection
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    user++;
-
-    console.log("#"+user+" Connected to WebSocket.")
     
-    socket.on("message", (msg) => {
-        allSockets.forEach((skt) => {
-            skt.send("Msg from Server: "+msg.toString());
-        })
+    // on message
+    socket.on("message", (message) => {
+        // convert message as string to json
+        console.log("parsed")
+        const parsedMessage = JSON.parse(message as unknown as string);
+        
+        console.log("parsedMessage: ", parsedMessage)
+        // If wants to join 
+        if(parsedMessage.type == "join"){
+            allSockets.push({
+                socket: socket,
+                room: parsedMessage.payload.roomId
+            });
+        }
+        
+        // If wants to chat
+        if(parsedMessage.type == "chat"){
+            const currentUserRoom = allSockets.find((x) => x.socket == socket)?.room
+
+            allSockets.forEach((storedSocket) => {
+                if(storedSocket.room == currentUserRoom){
+                    storedSocket.socket.send(parsedMessage.payload.message);
+                }
+            });
+        }
+
     });
 
-
     socket.on("close", () => {
-        allSockets = allSockets.filter(x => x !== socket)
+        allSockets = allSockets.filter((x) => x.socket != socket);
     })
 });
-
