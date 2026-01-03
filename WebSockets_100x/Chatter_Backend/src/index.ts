311d3e047453905e82f8290f8cf9496ddd44fe99
Rushikesh Borkar <borkarananta028@gmail.com>
2026-01-03 16:43:57 +0530

    broadcast server added

--- WebSockets_100x/Chatter_Backend/src/index.ts
+++ WebSockets_100x/Chatter_Backend/src/index.ts
@@ -1,26 +1,55 @@
-import { WebSocketServer } from "ws";
+// import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080});
+// const wss = new WebSocketServer({ port: 8080});
 
-wss.on("connection",  (socket) => {
-    console.log("Someone connected !");
+// wss.on("connection",  (socket) => {
+//     console.log("Someone connected !");
 
-    socket.on("message", (event) => {
-        const realMessage = event.toString();
-        setTimeout(()=>{
-            if(realMessage.toLowerCase() === "ping"){
-                socket.send("Pong");
-                console.log("Pong")
-            }else{
-                socket.send(realMessage);
-                console.log(realMessage);
-            }
-        }, 100)
-    });
+//     socket.on("message", (event) => {
+//         const realMessage = event.toString();
+//         setTimeout(()=>{
+//             if(realMessage.toLowerCase() === "ping"){
+//                 socket.send("Pong");
+//                 console.log("Pong")
+//             }else{
+//                 socket.send(realMessage);    
+//                 console.log(realMessage);
+//             }
+//         }, 100)
+//     });
 
 
-});
+// });
 
 
 // Server --> Client  done
 // Client --> Server  done
+
+// --------- Chatter: Broadcast Chat Application ------------
+
+
+import { WebSocketServer, WebSocket } from "ws";
+
+const wss = new WebSocketServer({port: 8080});
+
+let allSockets: WebSocket[] = [];
+let user = 0;
+
+wss.on("connection", (socket) => {
+    allSockets.push(socket);
+    user++;
+
+    console.log("#"+user+" Connected to WebSocket.")
+    
+    socket.on("message", (msg) => {
+        allSockets.forEach((skt) => {
+            skt.send("Msg from Server: "+msg.toString());
+        })
+    });
+
+
+    socket.on("close", () => {
+        allSockets = allSockets.filter(x => x !== socket)
+    })
+});
+
